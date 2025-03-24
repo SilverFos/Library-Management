b048f6da55e66a0229ed5a98b3ba18718f01b75f
Sanal VS <95088389+SilverFos@users.noreply.github.com>
2025-03-24 18:32:18 +0530

    hidden details in db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'SilverFos',
-    password: 'Yotequerotanto@2005',
+    user: '*****',
+    password: '*****',
     database: 'Library_management'
 });
 connection.connect((err) =>{
